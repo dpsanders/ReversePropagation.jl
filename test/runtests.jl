864be9041012df8b7cd02fa2e9cffe279ae98feb
David P. Sanders <dpsanders@gmail.com>
2020-12-02 18:25:51 -0500

    Fix and add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,8 +10,12 @@ using Test
 
         @testset "Single variable" begin
             f( (x, ) ) = x + 1
-            g = gradient( (x), f)
+            g = gradient( (x,), f )
             @test g(3) == (4, (1,))
+
+            f2( (x, ) ) = x^2 + x
+            g2 = gradient( (x,), f2 )
+            @test g2(3) == (12, (7,))
         end
 
         @testset "Two variables" begin
@@ -19,6 +23,11 @@ using Test
             vars = @variables x, y
             ∇h = gradient( (x, y), h )
             @test ∇h( (1, 2) ) == (9, (6, 6))
+
+            h2( (a, b) ) = a^2 + b^2
+            vars = @variables x, y
+            ∇h2 = gradient( (x, y), h2 )
+            @test ∇h2( (1, 2) ) == (5, (2, 4))
         end
 
 
