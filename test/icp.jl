369114382acc73cb9462f61866bb9ed063bae411
David Sanders <dpsanders@gmail.com>
2024-08-25 19:01:35 -0500

    Improve / correct tests

--- test/icp.jl
+++ test/icp.jl
@@ -1,6 +1,8 @@
 using IntervalContractors
-using IntervalArithmetic
+using IntervalArithmetic, IntervalArithmetic.Symbols
 
+eq(a, b) = isequal_interval(bareinterval(a), bareinterval(b))
+eq(a::Tuple, b::Tuple) = all(eq.(a, b))
 
 @testset "forward_backward_contractor" begin
 
@@ -8,24 +10,50 @@ using IntervalArithmetic
     @variables a  # parameter
 
     C = forward_backward_contractor(x + 1, x)
-    @test C(IntervalBox(-10..10), 2..3)[1] == (1..2, )
+    @test eq(C(IntervalBox(-10..10), 2..3)[1], (1..2, ))
 
-    C = forward_backward_contractor(x + (2..3), x)
-    @test C(IntervalBox(-10..10), 5..6)[1] == (2..4, )
-
-    C = forward_backward_contractor(x + (2..3), x)
-    @test C(IntervalBox(3..10), 5..6)[1] == (3..4, )
 
     ex = x^2 + y^2
 
     C = forward_backward_contractor(ex, vars)
-    @test C(IntervalBox(-10..10, -10..10), 0..1) == ( (-1..1, -1..1), 0..200 )
+    @test eq(C(IntervalBox(-10..10, -10..10), 0..1), ( (-1..1, -1..1), 0..200 ))
+end
+
+@testset "forward_backward_contractor with parameter" begin
+
+    ex = x + a
+
+    C = forward_backward_contractor(ex, [x], [a])
+
+    @test eq(C(IntervalBox(-10..10), 5..6, 2..3), ( (2..4, ), (-8..13) ) )
 
 
     ex = x^2 + a * y^2
 
     C = forward_backward_contractor(ex, vars, [a])
-    @test C(IntervalBox(-10..10, -10..10), 0..1, 1..1) == ( (-1..1, -1..1), 0..200 )
+    @test eq(C(IntervalBox(-10..10, -10..10), 0..1, 1..1), ( (-1..1, -1..1), 0..200 ))
+end
 
+@testset "bare intervals"
+
+    ex = x^ExactReal(2) + y^ExactReal(2)
+
+    C = forward_backward_contractor(ex, vars)
+    X = IntervalBox(bareinterval(-10..10), 2)
+    constraint = bareinterval(0..1)
+
+    @test eq(C(X, constraint), ( (bareinterval(-1..1), bareinterval(-1..1)), bareinterval(0..200) ) )
+
+
+    # with parameter:
+    # ex = x^ExactReal(2) + a * y^ExactReal(2)
+
+    # C = forward_backward_contractor(ex, vars, [a])
+
+    # X = IntervalBox(bareinterval(-10..10), 2)
+    # constraint = bareinterval(0..1)
+
+    # @test eq(C(X, constraint, [bareinterval(1..1)]),
+    #     ( (bareinterval(-1..1), bareinterval(-1..1)), bareinterval(0..200) ) )
 
 end
